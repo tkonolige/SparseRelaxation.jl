91cf9c260115c6561334353a6134666271322535
Tristan Konolige <tristan.konolige@gmail.com>
2016-10-12 14:36:46 -0600

    fix up tests

--- src/SparseRelaxation.jl
+++ src/SparseRelaxation.jl
@@ -14,7 +14,11 @@ Return a vector that is the result of applying one iteration of Gauss Seidel to
 triangular part of `A` and `U` is the strictly upper triangular part of `A`. If
 `backwards` is true, then a backwards sweep is performed. `A` is assumed to be symmetric. If not, then `gauss_seidel(A)` is really Gauss-Seidel applied to `A^T`.
 """
-function gauss_seidel(A :: SparseMatrixCSC, x :: Vector, b :: Vector, backwards :: Bool = false)
+function gauss_seidel{T}( A :: SparseMatrixCSC{T}
+                        , x :: Vector{T}
+                        , b :: Vector{T}
+                        , backwards :: Bool = false
+                        ) :: Vector{T}
     x_new = copy(x)
     gauss_seidel!(A, x_new, b, backwards)
     x_new
@@ -29,7 +33,7 @@ function gauss_seidel!{T}( A :: SparseMatrixCSC{T}
                          , x :: Vector{T}
                          , b :: Vector{T}
                          , backwards::Bool = false
-                         )
+                         ) :: Vector{T}
     rows = rowvals(A)
     vals = nonzeros(A)
     m, n = size(A)
@@ -39,7 +43,7 @@ function gauss_seidel!{T}( A :: SparseMatrixCSC{T}
 
         js = nzrange(A, col)
         if backwards
-            reverse(js)
+            js = reverse(js) # js is range, reverse is cheap
         end
         @inbounds for j in js
             row = rows[j]
@@ -53,15 +57,21 @@ function gauss_seidel!{T}( A :: SparseMatrixCSC{T}
 
         @inbounds x[col] = (b[col] - colsum) / diag
     end
+
+    x
 end
 
-function weighted_jacobi!(A :: SparseMatrixCSC, x :: Vector, b :: Vector, weight = 2/3)
+function weighted_jacobi!(A :: SparseMatrixCSC, x :: Vector, b :: Vector, weight = 2/3) :: Vector{T}
     Di = spdiagm(map(x -> 1/x, diag(A)))
     R = A - spdiagm(diag(A))
     x[:] = weight * Di * (b - R*x) + (1 - weight) * x
 end
 
-function weighted_jacobi(A :: SparseMatrixCSC, x :: Vector, b :: Vector, weight = 2/3)
+function weighted_jacobi{T}( A :: SparseMatrixCSC{T}
+                           , x :: Vector{T}
+                           , b :: Vector{T}
+                           , weight = 2/3
+                           ) :: Vector{T}
     x_new = copy(x)
     weighted_jacobi!(A, x_new, b, weight)
     x_new
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,32 @@
 using SparseRelaxation
 using Base.Test
 
-# test zero input
-@test all(x -> x == 0, gauss_seidel(speye(100), zeros(100), zeros(100)))
+function conv_test(smoother)
+    # test zero input
+    @test all(x -> x == 0, smoother(speye(100), zeros(100), zeros(100)))
 
-# test b = all ones
-@test all(x -> x == 1, gauss_seidel(speye(100), zeros(100), ones(100)))
+    # test convergence
+    function converges()
+        A = spdiagm(ones(99), 1, 100, 100)
+        A = A + transpose(A)
+        L = spdiagm(vec(sum(A, 1))) - A
+        x = zeros(size(L, 1))
+        b = rand(size(L, 1))
+        b = b - mean(b)
+        x1 = smoother(L,x,b)
+        norm(b - L*x1) < norm(b - L*x)
+    end
 
-# test convergence
-function converges(backwards)
-    A = spdiagm(ones(99), 1, 100, 100)
-    A = A + transpose(A)
-    L = spdiagm(vec(sum(A, 1))) - A
-    x = zeros(size(L, 1))
-    b = rand(size(L, 1))
-    b = b - mean(b)
-    x1 = gauss_seidel(L,x,b,backwards)
-    norm(b - L*x1) < norm(b - L*x)
+    # check convergence
+    @test converges()
 end
 
-# check forward convergence
-@test converges(true)
-
-#check backwards convergence
-@test converges(false)
+@testset "gauss_seidel forwards" begin
+    conv_test((A,x,b)->gauss_seidel(A,x,b,false))
+end
+@testset "gauss_seidel backwards" begin
+    conv_test((A,x,b)->gauss_seidel(A,x,b,true))
+end
+@testset "weighted_jacobi" begin
+    conv_test(weighted_jacobi)
+end
